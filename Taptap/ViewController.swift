41a7631e7ac6712500f488d2712cd79079502f3d
DEREK CONWAY <D@dereks-mbp>
2016-04-12 18:09:23 -0500

    Initial Commit

---
+++ Taptap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taptap
+//
+//  Created by D on 4/12/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
